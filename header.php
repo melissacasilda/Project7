6f96c8b5de4d08566ef0e9d72cfd2be5152710c2
Melissa Gonsalves <mcgonsalves@gmail.com>
2016-05-31 17:03:14 -0400

    Initial Commit

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300|Montserrat|Crimson+Text:400italic' rel='stylesheet' type='text/css'>
   <!-- stylesheets should be enqueued in functions.php -->
   <?php wp_head(); ?>
 </head>
